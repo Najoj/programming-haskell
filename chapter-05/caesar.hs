cb7094316b4514301331a72322ffbba7dc887543
Johan Öhlin <johanohlin@gmx.com>
2020-03-08 22:50:14 +0100

    revert staart of making cipher work with upper-case

--- chapter-05/caesar.hs
+++ chapter-05/caesar.hs
@@ -24,10 +24,8 @@ int2let :: Int -> Char
 int2let n = chr (ord 'a' + n)
 
 shift :: Int -> Char -> Char
-shift n c | isLower c = int2let ((let2int c + n) `mod` alen)
+shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
           | otherwise = c
-            where 
-                alen = ord 'z' - ord 'a' + 1
 
 
 encode :: Int -> String -> String 
